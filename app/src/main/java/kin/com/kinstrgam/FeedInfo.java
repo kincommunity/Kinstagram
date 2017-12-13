1e6ac6445724d2e7d78c37b9df971c11d7018e8d
Kyungsoo Hong <kyungsoo@rounds.com>
2017-12-13 16:42:00 +0900

    Kyungsoo/ui for images list (#4)
    * Changed UI, Upload images to FireBase and display the uploaded image on screen
    
    * image feed ui

---
+++ app/src/main/java/kin/com/kinstrgam/FeedInfo.java
@@ -0,0 +1,44 @@
+package kin.com.kinstrgam;
+
+/**
+ * Created by kyungsoohong on 12/13/17.
+ */
+
+public class FeedInfo {
+    private String name, imageUrl, timeStamp;
+
+    public FeedInfo() {
+
+    }
+
+    public FeedInfo(String name, String imageUrl, String timeStamp) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.timeStamp = timeStamp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(String timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+}
--- app/src/main/java/kin/com/kinstrgam/MainActivity.java
+++ app/src/main/java/kin/com/kinstrgam/MainActivity.java
@@ -1,30 +1,52 @@
 package kin.com.kinstrgam;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.ResultCodes;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
     private static final int RC_SIGN_IN = 123;
 
 
     private static final String APPLICATION_ID = "kinstagramandroid";
     private static final String API_KEY = "fakeKey";
     private static final String FIREBASE_APP_NAME = "firebaseAppName";
+
+
 
     public static FirebaseApp initializeApp(Context context) {
         try {
@@ -91,6 +113,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         Log.d("kinstagram","onActivityResult " + requestCode + " result: " + resultCode + " data: "+  data);
@@ -102,12 +127,18 @@ public class MainActivity extends AppCompatActivity {
             if (resultCode == ResultCodes.OK) {
                 // Successfully signed in
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                // ...
+                Toast.makeText(getApplicationContext(), "Registration Completed: " + user.getDisplayName(), Toast.LENGTH_LONG).show();
+                //setContentView(R.layout.activity_main);
+
+
+                Intent intent = new Intent(MainActivity.this, FeedListActivity.class);
+                startActivity(intent);
+
+
             } else {
                 // Sign in failed, check response for error code
                 // ...
             }
         }
     }
-
 }
