8f6d3fffb6826b42cf60d16d57fa209f0399570e
YohayBar <yohay@kik.com>
2017-12-14 13:18:56 +0200

    add kin sdk + get Kin

--- app/src/main/java/kin/com/kinstrgam/FeedListActivity.java
+++ app/src/main/java/kin/com/kinstrgam/FeedListActivity.java
@@ -18,6 +18,10 @@ import android.widget.ImageView;
 import android.widget.Toast;
 import android.Manifest;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -32,6 +36,7 @@ import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -39,6 +44,12 @@ import java.util.List;
 import java.util.Locale;
 
 import kin.com.kinstrgam.Camera.CameraActivity;
+import kin.com.kinstrgam.model.Wallet;
+import kin.sdk.core.Balance;
+import kin.sdk.core.KinAccount;
+import kin.sdk.core.KinClient;
+import kin.sdk.core.ResultCallback;
+import kin.sdk.core.ServiceProvider;
 
 public class FeedListActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
 
@@ -48,8 +59,12 @@ public class FeedListActivity extends AppCompatActivity implements ActivityCompa
 
     private static final int PERMISSION_REQUEST_CAMERA = 0;
     private static final int PERMISSION_REQUEST_STORAGE = 1;
+    private KinClient kinClient;
+    private KinAccount kinAccount;
 
     private static final String DATABASE_PATH = "All_Image_Uploads_Database";
+    public static final String URL_GET_KIN = "http://kin-faucet.rounds.video/send?public_address=";
+
 
     FirebaseStorage storage;
     DatabaseReference databaseReference;
@@ -157,6 +172,19 @@ public class FeedListActivity extends AppCompatActivity implements ActivityCompa
             }
         });
         mCameraButton.setClickable(false);
+        ServiceProvider serviceProvider =  new ServiceProvider("http://parity.rounds.video:8545", ServiceProvider.NETWORK_ID_ROPSTEN);
+
+        try {
+            kinClient = new KinClient(getApplicationContext(), serviceProvider);
+            if(kinClient.hasAccount()){
+                kinAccount = kinClient.getAccount();
+            } else {
+                kinAccount = kinClient.createAccount(Wallet.PASSPHRASE);
+            }
+        }catch(Exception e){
+
+        }
+        getKin(kinAccount);
     }
 
     public void requestPermissions(){
@@ -251,6 +279,37 @@ public class FeedListActivity extends AppCompatActivity implements ActivityCompa
             }
         }
 
+    }
+
+    private void getKin(KinAccount account) {
+        if (account != null) {
+            account.getBalance(new ResultCallback<Balance>() {
+                @Override
+                public void onResult(Balance balance) {
+                    if(balance.value().compareTo(BigDecimal.TEN) < -1){
+                        //do noting
+                    }else {
+                        final String publicAddress = account.getPublicAddress();
+                        final String url = URL_GET_KIN + publicAddress;
+                        final RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                        final StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                                response -> {
+                                    Log.d(TAG, "getKin: success");
+                                },
+                                e -> {
+                                    Log.d(TAG, "getKin: error");
+                                });
+                        stringRequest.setShouldCache(false);
+                        queue.add(stringRequest);
+                    }
+                }
+
+                @Override
+                public void onError(Exception e) {
+                    Log.d(TAG, "getKin getbalance: error");
+                }
+            });
+        }
     }
 
     public void uploadImage() {
--- app/src/main/java/kin/com/kinstrgam/MainActivity.java
+++ app/src/main/java/kin/com/kinstrgam/MainActivity.java
@@ -7,22 +7,39 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.ResultCodes;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.Arrays;
 import java.util.List;
 
+import kin.com.kinstrgam.model.User;
+import kin.com.kinstrgam.model.Wallet;
+import kin.sdk.core.KinAccount;
+import kin.sdk.core.KinClient;
+import kin.sdk.core.ServiceProvider;
+import com.android.volley.RequestQueue;
+import com.android.volley.Request;
 
 public class MainActivity extends AppCompatActivity  {
 
     private static final String TAG = "MainActivity";
 
     private static final int RC_SIGN_IN = 123;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,11 +73,39 @@ public class MainActivity extends AppCompatActivity  {
 
             if (resultCode == ResultCodes.OK) {
                 // Successfully signed in
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 Toast.makeText(getApplicationContext(), "Registration Completed: " + user.getDisplayName(), Toast.LENGTH_LONG).show();
+                ServiceProvider serviceProvider =  new ServiceProvider("http://parity.rounds.video:8545", ServiceProvider.NETWORK_ID_ROPSTEN);
+                try{
+                    KinClient kinClient = new KinClient(getApplicationContext(), serviceProvider);
+                    KinAccount kinAccount = kinClient.createAccount(Wallet.PASSPHRASE);
+                   // getKin(kinAccount);
+                    Log.d(TAG, "onActivityResult: public address" +kinAccount.getPublicAddress());
+                    final Wallet wallet = new Wallet(kinAccount.getPublicAddress());
+                    final DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+                    databaseReference.child("Users").child(user.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            if(databaseReference == null){
+                                databaseReference.child("Users").child(user.getUid()).setValue(new User(user.getDisplayName()));
+                                databaseReference.child("Wallets").child(user.getUid()).setValue(wallet);
+                            }
+                            Intent intent = new Intent(MainActivity.this, FeedListActivity.class);
+                            startActivity(intent);
+                        }
 
-                Intent intent = new Intent(MainActivity.this, FeedListActivity.class);
-                startActivity(intent);
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+
+
+
+                }catch (Exception e){
+
+                }
+
 
             } else {
                 // Sign in failed, check response for error code
